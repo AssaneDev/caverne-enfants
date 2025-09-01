a356b4ba7ddf85137ac5ca7e73b6fe9bfbfa20ec
Assane <adiop6225@gmail.com>
2025-09-01 11:50:11 +0200

    bug ajout artiste

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -22,6 +23,17 @@ class Artist extends Model implements HasMedia
     protected $casts = [
         'links' => 'array',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($artist) {
+            if (empty($artist->slug)) {
+                $artist->slug = Str::slug($artist->name);
+            }
+        });
+    }
 
     public function artworks(): HasMany
     {
